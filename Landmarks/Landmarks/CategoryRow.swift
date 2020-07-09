122c42f7a956f59d63a9bb70b08cd5baf09c58a1
ZihouWong <wongzihou1997@gmail.com>
2020-07-09 08:32:18 +0800

    Section 5

---
+++ Landmarks/Landmarks/CategoryRow.swift
@@ -0,0 +1,62 @@
+//
+//  CategoryRow.swift
+//  Landmarks
+//
+//  Created by Zihou Wong on 7/8/20.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct CategoryRow: View {
+    var categoryName: String
+    var items: [Landmark]
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(self.categoryName)
+                .font(.headline)
+                .padding(.leading, 10)
+                .padding(.top, 5)
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(alignment: .top, spacing: 0 ) {
+                    ForEach(self.items) { landmark in
+                        NavigationLink(
+                            destination: LandmarkDetail(
+                                landmark: landmark
+                            )
+                        ) {
+                            CategoryItem(landmark: landmark)
+                        }
+                    }
+                }
+            }
+            .frame(height: 185)
+        }
+    }
+}
+
+struct CategoryItem: View {
+    var landmark: Landmark
+    var body: some View {
+        VStack {
+            landmark.image
+                .resizable()
+                .renderingMode(.original)
+                .frame(width: 155, height: 155)
+                .cornerRadius(5)
+            Text(landmark.name)
+                .foregroundColor(.primary)
+                .font(.caption)
+        }
+        .padding(.leading, 15)
+    }
+}
+struct CategoryRow_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryRow(
+            categoryName: landmarkData[0].category.rawValue,
+            items: Array(landmarkData.prefix(4)))
+    }
+}
